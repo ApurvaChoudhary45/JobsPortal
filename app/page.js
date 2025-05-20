75809cb49e550725dd7d283eb75ac8185c5331bc
ApurvaChoudhary45 <badassmannu45@gmail.com>
2025-05-20 07:23:26 +0530

    Login issue resolved

--- app/page.js
+++ app/page.js
@@ -12,23 +12,25 @@ export default function Home() {
   if(session){
     return (
       <>
+      <div>
       <div className="selection:bg-blue-400 selection:text-pink-300">
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div></div>
       <Navbar />
       <JobDesc />
-      
+      </div>
       </>
     );
   }
   return (
     <>
+    <div>
     <div className="selection:bg-blue-400 selection:text-pink-300">
     <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div></div>
     <Navbar />
     <Search />
     <About />
     <Footer />
-    
+    </div>
     </>
   );
 }
--- components/Search.jsx
+++ components/Search.jsx
@@ -35,7 +35,7 @@ const Search = () => {
     <input
       type="text"
       placeholder="Search for jobs, companies, or keywords"
-      className="w-full px-4 py-2 border border-black-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e)=>setInput(e.target.value)} value={Input}
+      className="w-full px-4 py-2 border border-black-300 rounded-l-md focus:outline-none focus:ring-2 text-black focus:ring-blue-500" onChange={(e)=>setInput(e.target.value)} value={Input}
     />
     <button
       className="px-4 bg-blue-400 text-white rounded-r-md hover:bg-blue-700 cursor-pointer" onClick={search} 
